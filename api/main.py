6d183749171af9e939295ff01327b449d247949f
Abdulhade <contact@abdulhade.com>
2026-04-28 23:16:57 +0300

    Fixed the on startup event and used lifespan

--- api/main.py
+++ api/main.py
@@ -3,14 +3,15 @@ from fastapi import FastAPI
 from core.database import create_tables
 from routes import auth, products, users
 
+async def lifespan(app):
+    await create_tables()
+    print("Created the tables.")
+    yield
 
-app = FastAPI(title="BIA Placeholder API")
+app = FastAPI(title="BIA Placeholder API", lifespan=lifespan)
+
+
 
 app.include_router(auth.router)
 app.include_router(users.router)
 app.include_router(products.router)
-
-
-@app.on_event("startup")
-async def on_startup():
-    await create_tables()
